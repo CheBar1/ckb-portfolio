7862684768b33b35a5dcc33b2b0fbff99d3733a8
CheBar1 <chebarclay@live.com.au>
2023-03-28 16:07:41 +1030

    Added info to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,37 @@
-import React from 'react'
+import React from "react";
 import "../index.css";
 // Use rafce as shortcut to create React arrow export function
 
 const Footer = () => {
   return (
     <footer className="footer">
-    <a href="https://github.com/CheBar1"><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="Github" className="icon"/></a>
-    <a href="https://www.linkedin.com/in/cheryl-barclay-531985252/"><img src="https://img.icons8.com/fluent/48/000000/linkedin.png" alt="LinkedIn" className="icon"/></a>
-    <a href="https://www.instagram.com/cheryl.k.barclay/"><img src="https://img.icons8.com/fluency/48/null/instagram-new.png" alt="Instagram" className="icon"/></a>
-</footer>
-  )
-}
+      <h2>Designed and Developed by Cheryl Barclay</h2>
+      <h3>Copyright © 2023</h3>
+      <section>
+      <a href="https://github.com/CheBar1">
+        <img
+          src="https://img.icons8.com/color/48/000000/github--v1.png"
+          alt="Github"
+          className="icon"
+        />
+      </a>
+      <a href="https://www.linkedin.com/in/cheryl-barclay-531985252/">
+        <img
+          src="https://img.icons8.com/fluent/48/000000/linkedin.png"
+          alt="LinkedIn"
+          className="icon"
+        />
+      </a>
+      <a href="https://www.instagram.com/cheryl.k.barclay/">
+        <img
+          src="https://img.icons8.com/fluency/48/null/instagram-new.png"
+          alt="Instagram"
+          className="icon"
+        />
+      </a> 
+      </section> 
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
